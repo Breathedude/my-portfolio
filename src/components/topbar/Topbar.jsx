f440a5a8a0f9cfafeb4ded7313e12900680a9a73
B Rachitha <rachitha@adobe.com>
2021-10-11 19:26:02 +0530

    Creating my profile

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./topbar.scss"
+
+export default function Topbar() {
+  return (
+    <div className = "topbar" id="topbar">
+        <div className = "wrapper">
+            <div className = "left"> 
+                <a href="#intro">Intro</a>
+            </div>
+            <div className = "right"></div>
+        </div>
+      
+    </div>
+  )
+}
